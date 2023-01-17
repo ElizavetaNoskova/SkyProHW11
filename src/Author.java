5b7ed59663d7a1cdcbd9bf5b7e3d3dd340cf8832
ElizavetaNoskova <wliza68@gmail.com>
2023-01-17 21:22:27 +0300

    First commit

---
+++ src/Author.java
@@ -0,0 +1,11 @@
+public class Author {
+     private String authorName;
+
+    public String getName() {
+        return authorName;
+    }
+    public Author(String authorName){
+        this.authorName = authorName.toString();
+    }
+}
+
---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private String title;
+    private Author authorName;
+    private int publishingYear;
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+    public Author getAuthorName() {
+        return authorName;
+    }
+    public Book(String title, Author authorName, int publishingYear){
+        this.title = title;
+        this.authorName = authorName;
+        this.publishingYear = publishingYear;
+    }
+}
